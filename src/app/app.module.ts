8a29524cc2ab688f74e8e93ad6e56ba8bfd79739
Lilupa <lilupa.karunarathne@fronde.com>
2019-03-08 20:58:10 +1300

    Added custom calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -12,13 +13,20 @@ import { LoginComponent } from './login/login.component';
 import { ContactCoordinatorComponent } from './contact-coordinator/contact-coordinator.component';
 import { DanaRosterComponent } from './dana-roster/dana-roster.component';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { NishCalendarModule } from './nish-calendar/nish-calendar-module';
+
 @NgModule({
   declarations: [AppComponent, LoginComponent, ContactCoordinatorComponent, DanaRosterComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    NgbModule,
+    AppRoutingModule,
+    NishCalendarModule
   ],
   providers: [
     StatusBar,
